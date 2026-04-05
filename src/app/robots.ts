9bfd64b9a1848dd5d18e63b936cb63d44945ca50
waste3d <kolya@waste3d.su>
2026-04-05 19:34:46 +0300

    feat: metadata

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://sledix.com/sitemap.xml',
+    sitemap: 'https://sledix.tech/sitemap.xml',
   }
 }
